63c6b9f283079c40a7e43cacd845d3fff19e360d
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 22:14:46 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,7 +11,7 @@ class Song < ActiveRecord::Base
 
     if self.artist != drake
       self.artist = drake
-
+      binding.pry
 
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
