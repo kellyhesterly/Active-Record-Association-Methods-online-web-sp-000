52794bb223504333c7e8663a73ba74bcacef5648
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 22:05:15 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.class.all
+    artist.all.name
     binding.pry
     # return an array of strings containing every musician's name
   end
