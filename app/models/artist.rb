e3d856132d58ea86292fb3512a7dfd8ce146588b
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 21:56:00 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,8 +2,11 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+  end
+
   def get_genre_of_first_song
-    self.songs.first.genre_id
+    self.songs
   end
 
   def song_count
