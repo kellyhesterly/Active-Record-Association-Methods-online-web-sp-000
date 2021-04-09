2008f422a38f7b37add86ce4a7a54cb2e9fe6e54
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 22:12:58 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     drake = Artist.new(name: "Drake")
-    
+
     if self.artist != drake
     drake = Artist.new(name: "Drake")
     self.artist = drake
